afada0468ab078d7e8b05e8e10c6e6e154ec8e44
Omkar Ubale <oubale@gmail.com>
2020-09-16 00:46:34 +0530

    MIGRATION: Added CreateUserTable Migration

---
+++ OU.CMS.Domain/Migrations/202009151913383_CreateUserTable.cs
@@ -0,0 +1,31 @@
+namespace OU.CMS.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateUserTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        FullName = c.String(),
+                        ShortName = c.String(),
+                        Email = c.String(),
+                        UserType = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
