742096f040feeb0d48f4b176dfc943a2c8b491c1
Omkar Ubale <oubale@gmail.com>
2020-09-15 22:12:33 +0530

    added Common, Models, Domain and API Projects

---
+++ OU.CMS.Models/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OU.CMS.Models
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
