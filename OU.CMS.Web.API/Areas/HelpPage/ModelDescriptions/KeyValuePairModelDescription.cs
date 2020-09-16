74efedd31e1fc68e752e3793845ee4dad718ef04
Omkar Ubale <oubale@gmail.com>
2020-09-16 18:09:52 +0530

    Cleaned up Web.API Project to start creating API, Added Core Project for Commands and Queries

--- OU.CMS.Web.API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace OU.CMS.Web.API.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
--- OU.CMS.Web.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace OU.CMS.Web.API.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
